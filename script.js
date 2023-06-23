f47568c6687efa42b92c85b0fb61cf25fc86e482
jonpfluger <jonpfluger718@gmail.com>
2023-06-22 20:00:43 -0500

    sets up theme switcher button

--- script.js
+++ script.js
@@ -1,33 +1,12 @@
-// var abilities = [
-//     "select",
-//     "create",
-//     "style",
-//     "animate",
-//     "traverse",
-//     "event listen"
-// ]
+var alertButtonEl = $('#alert-btn');
+var themeButtonEl = $('#theme-btn');
+var lotteryButtonEl = $('#lottery-btn');
+var lotteryNumberEl = $('#lottery-number');
+var refreshButtonEl = $('#refresh-btn');
 
-// var $ul = $("<ul>")
+// light theme state
+var isDark = true;
 
-// for (var i = 0; i < abilities.length; i++) {
-//     $("<li>")
-//         .text( abilities[i] )
-//         .css("margin-bottom", "20px")
-//         .appendTo($ul)
-// }
-
-// $ul.appendTo("body")
-
-var $root = $("<div>").attr("id", "root").appendTo("body")
-
-var $author = $("<p>")
-    .text("~ Carol Dweck")
-    .addClass("plain")
-
-var $quote = $("<h1>")
-    .text(`"Love Challenges, Be Intrigued by Mistakes, Enjoy Effort, and Keep Learning."`)
-    .addClass("fancy")
-
-$quote.append($author)
-
-$root.append($quote)
+themeButtonEl.on("click", function() {
+    
+})
