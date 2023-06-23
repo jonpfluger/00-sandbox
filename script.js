0bf328d207704916f03987b6a2942e1c89da4880
jonpfluger <jonpfluger718@gmail.com>
2023-06-22 19:25:27 -0500

    looping with jquery

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+var abilities = [
+    "select",
+    "create",
+    "style",
+    "animate",
+    "traverse",
+    "event listen"
+]
 
+var $ul = $("<ul>")
 
+for (var i = 0; i < abilities.length; i++) {
+    $("<li>")
+        .text( abilities[i] )
+        .appendTo($ul)
+}
 
-$("body").append($("<h1>").text("Hello jQuery!"))
-
-
+$ul.appendTo("body")
