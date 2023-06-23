c2325dab99f39cd13979e2f1df54364604491374
jonpfluger <jonpfluger718@gmail.com>
2023-06-22 19:55:17 -0500

    practice

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-var abilities = [
-    "select",
-    "create",
-    "style",
-    "animate",
-    "traverse",
-    "event listen"
-]
+// var abilities = [
+//     "select",
+//     "create",
+//     "style",
+//     "animate",
+//     "traverse",
+//     "event listen"
+// ]
 
-var $ul = $("<ul>")
+// var $ul = $("<ul>")
 
-for (var i = 0; i < abilities.length; i++) {
-    $("<li>")
-        .text( abilities[i] )
-        .css("margin-bottom", "20px")
-        .appendTo($ul)
-}
+// for (var i = 0; i < abilities.length; i++) {
+//     $("<li>")
+//         .text( abilities[i] )
+//         .css("margin-bottom", "20px")
+//         .appendTo($ul)
+// }
 
-$ul.appendTo("body")
+// $ul.appendTo("body")
+
+var $root = $("<div>").attr("id", "root").appendTo("body")
+
+var $author = $("<p>")
+    .text("~ Carol Dweck")
+    .addClass("plain")
+
+var $quote = $("<h1>")
+    .text(`"Love Challenges, Be Intrigued by Mistakes, Enjoy Effort, and Keep Learning."`)
+    .addClass("fancy")
+
+$quote.append($author)
+
+$root.append($quote)
