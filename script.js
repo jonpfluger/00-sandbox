bf6a0373f458d441f347d64e87fe2c372908beab
jonpfluger <jonpfluger718@gmail.com>
2023-06-29 19:53:22 -0500

    more fetch API notes

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 fetch("https://api.github.com/users")
     .then(function(response) {
-        // console.log(response)
         return response.json()
+    })
+    .then(function(json) {
+        for (var i = 0; i < json.length; i++) {
+            var user = json[i]
+            // create username h2
+            var h2 = document.createElement("h2")
+            h2.innerText = user.login
+            document.body.appendChild(h2)
+            // create avatar img
+            var img = document.createElement('img')
+            img.src = user.avatar_url
+            document.body.appendChild(img)
+        }
     })
