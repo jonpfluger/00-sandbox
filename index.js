83e266e016f1fb8d4c5243b1b66b0e94b1b64a8c
jonpfluger <jonpfluger718@gmail.com>
2023-07-25 19:53:20 -0500

    old way

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-// ES5
-// function makeGreeting(message, name) {
-//     return message + ", " + name + "!"
-// }
+var dog = {
+    name: "Zues",
+    age: 14,
+    sound: "bark",
+    makeSoundAfterTimeout() {
+        var self = this
+        setTimeout(function() {
+            console.log(self.sound)
+        }, 2000)
+    }
+}
 
-// var greeting1 = makeGreeting("Hello", "Jon")
-// var greeting2 = makeGreeting("Hi", "Josh")
-
-// console.log(greeting1, greeting2)
-
-
-// ES6
-
-var makeGreeting = (message, name) => `${message}, ${name}!`
-
-var greeting1 = makeGreeting("Hello", "Josh")
-var greeting2 = makeGreeting("Hi", "Jon")
-
-console.log(greeting1, greeting2)
+dog.makeSoundAfterTimeout()
