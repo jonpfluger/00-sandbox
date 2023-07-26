61d87d6e8c535239f6dd9aaad4c04bdcf35ca0a6
jonpfluger <jonpfluger718@gmail.com>
2023-07-25 20:58:33 -0500

    interpolation

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
-const apiUrl = 'https://api.openweathermap.org/data/3.0/onecall'
-const gravity = 10
-const isHungry = false
-const notDefined = undefined
+const name = 'Jon'
+const position = 'TA'
 
-// apiUrl = 'X'
-// gravity = 0
+const getRandomNum = () => Math.random()
 
-const animals = ['monkey', 'cat', 'zebra']
+const html = `<div>
+  <h2>Hello, I am ${name}</h2>
+  <p>I am a ${position} in the coding bootcamp.</p>
+  <p>${getRandomNum()}</p>
+</div>`
 
-// animals = [2, 3, 4] // cannot reassign
-
-// but we can modify
-// animals.push('lemur')
-const lastAnimal = animals.pop()
-// console.log(lastAnimal)
-
-const dog = {
-  name: 'Josh',
-  age: 14,
-}
-
-// dog = {
-//   name: 'test',
-//   age: 1
-// }
-
-dog.name = 'test'
-
-console.log(dog)
+console.log(html)
