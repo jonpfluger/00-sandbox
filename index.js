848b19a22c4826818a73cee1bbe95b357f06dcfa
jonpfluger <jonpfluger718@gmail.com>
2023-07-25 20:18:42 -0500

    const

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-// let x = 0
+const apiUrl = 'https://api.openweathermap.org/data/3.0/onecall'
+const gravity = 10
+const isHungry = false
+const notDefined = undefined
 
-// if (true) {
-//   let x = 10
+// apiUrl = 'X'
+// gravity = 0
+
+const animals = ['monkey', 'cat', 'zebra']
+
+// animals = [2, 3, 4] // cannot reassign
+
+// but we can modify
+// animals.push('lemur')
+const lastAnimal = animals.pop()
+// console.log(lastAnimal)
+
+const dog = {
+  name: 'Josh',
+  age: 14,
+}
+
+// dog = {
+//   name: 'test',
+//   age: 1
 // }
 
-// console.log(x)
+dog.name = 'test'
 
-let animals = ['monkey', 'cat', 'zebra']
-
-for (let i = 0; i < animals.length; i++) {
-  console.log(animals[i])
-}
-
-for (let i = 0; i < animals.length; i++) {
-  console.log(animals[i])
-}
+console.log(dog)
