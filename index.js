0e02ddad4bc166f0b6e6a2906bc18424c56313ca
jonpfluger <jonpfluger718@gmail.com>
2023-07-27 18:44:08 -0500

    process.argv

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-// Write a function that takes in an array of integers and the array with duplicates removed
+const name = process.argv[2]
+const favFood = process.argv[3]
 
-// Ex:
-// Input: [1,2,2,3]
-// Output: [1,2,3]
-
-// Input: [4,5,4,4,7,5]
-// Output: [4,5,7]
-
-// Input: [1,2,3,5]
-// Output: [1,2,3,5]
-
-
-const removeDuplicates = arr => arr.filter((num, index) => arr.indexOf(num) === index)
-
-console.log(removeDuplicates([1,2,2,3]))
-console.log(removeDuplicates([4,5,4,4,7,5]))
-console.log(removeDuplicates([1,2,3,5]))
+console.log(`My name is ${name}. My favorite food is ${favFood}.`)
