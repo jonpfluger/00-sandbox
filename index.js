3b052b1b51a04719b4d134336723fc69a8f1d95b
jonpfluger <jonpfluger718@gmail.com>
2023-08-01 19:28:36 -0500

    ES5 constructor functions

---
+++ index.js
@@ -0,0 +1,18 @@
+// ES5 Constructor Function (blueprint that creates objects)
+function Dog(name, age, breed) {
+    this.name = name
+    this.age = age
+    this.breed = breed
+    this.nap = function() {
+        console.log(`${this.name} Zzzzzzzz`)
+    }
+}
+
+// instantiation
+const mabel = new Dog('Mabel', 1, 'Old English Bulldog')
+const alpha = new Dog('Alpha', 3, 'Terrior Pitbull')
+const bruno = new Dog('Bruno', 1.5, 'Terrior Pitbull')
+
+mabel.nap()
+alpha.nap()
+bruno.nap()
