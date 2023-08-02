42c15692e3a89aedab618c68164fb19fbdb7ddbe
jonpfluger <jonpfluger718@gmail.com>
2023-08-01 21:57:45 -0500

    daily practice exercise algorithm

---
+++ index.js
@@ -0,0 +1,23 @@
+// Write a function that takes in a string and if the string is a string representation of a number, return the negative version of that number otherwise throw an Error.
+
+// Ex:
+// Input: '1'
+// Output: -1
+
+// Input: '4'
+// Output: -4
+
+// Input: 'cow'
+// Error
+
+const myFunction = str => {
+    const num = parseInt(str)
+    const result = num - (num * 2)
+    if (result) {
+        console.log(result)
+    } else {
+        console.log('error')
+    }
+}
+
+myFunction("7")
