ba71f1fcb53dd197cc45560eda25c6ed447d5360
jonpfluger <jonpfluger718@gmail.com>
2023-08-17 21:32:45 -0500

    pokemon api routes

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,3 +1,68 @@
 const router = require('express').Router()
+const connection = require('../db/connection')
+
+router.post('/', async (req, res) => {
+  const {name, type, moves, is_evolved, trainer_id} = req.body
+  try {
+    const result = await connection.promise().query(
+      `INSERT INTO pokemon (name, type, moves, is_evolved, trainer_id) VALUES (?, ?, ?, ?, ?);`,
+      [name, type, moves, is_evolved, trainer_id]
+    )
+    res.json(result)
+  } catch(err) {
+    res.status(500).json(err)
+  }
+})
+
+router.get('/', async (req, res) => {
+  try {
+    const result = await connection.promise().query(`SELECT * FROM pokemon;`)
+    res.json(result)
+  } catch(err) {
+    res.status(500).json(err)
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  const id = req.params.id
+  try {
+    const result = await connection.promise().query(
+      `SELECT * FROM pokemon WHERE id = ?;`,
+      id
+    )
+    res.json(result)
+  } catch(err) {
+    res.status(500).json(err)
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  const id = req.params.id
+  const {name, type, moves, is_evolved, trainer_id} = req.body
+  try {
+    const result = await connection.promise().query(
+      `UPDATE pokemon
+      SET name = ?, type = ?, moves =?, is_evolved = ?, trainer_id = ?
+      WHERE id = ?;`,
+      [name, type, moves, is_evolved, trainer_id, id]
+    )
+    res.json(result)
+  } catch(err) {
+    res.status(500).json(err)
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  const id = req.params.id
+  try {
+    const result = await connection.promise().query(
+      `DELETE FROM pokemon WHERE id = ?;`,
+      id
+    )
+    res.json(result)
+  } catch(err) {
+    res.status(500).json(err)
+  }
+})
 
 module.exports = router
