0d4bb71fbab4e3f71a11f1516e1896258c1ce5c7
jonpfluger <jonpfluger718@gmail.com>
2023-08-22 19:47:55 -0500

    sequelize

---
+++ server.js
@@ -0,0 +1,44 @@
+const { Sequelize, DataTypes, Model } = require('sequelize')
+
+const sequelize = new Sequelize('pokemon_db', 'root', 'root', {
+  host: 'localhost',
+  dialect: 'mysql'
+})
+
+
+// define models by using .define()
+const Moves = sequelize.define('Move', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  hp: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  }
+})
+
+// define models by extending model class
+class Trainer extends Model {}
+
+Trainer.init({
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  age: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  numBadges: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  // dateAdded: {
+  //   type: DataTypes.DATE,
+  //   defaultValue: DataTypes.NOW,
+  // },
+}, {
+  timestamps: true,
+})
