70bdd10365796d804a34938c2d29964c8554e4ae
jonpfluger <jonpfluger718@gmail.com>
2023-08-22 20:28:26 -0500

    pokemon models

---
+++ models/Pokemon.js
@@ -0,0 +1,37 @@
+const { DataTypes, Model } = require('sequelize')
+const sequelize = require('../db/connection')
+
+class Pokemon extends Model {}
+
+Pokemon.init({
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  type: {
+    type: DataTypes.STRING(30),
+    allowNull: false,
+  },
+  isEvolved: {
+    types: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  moves: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'Moves',
+      key: 'id',
+    },
+  },
+  trainerId: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    references: {
+      model: 'Trainers',
+      key: 'id',
+    },
+  },
+}, {
+  sequelize,
+  modelName: 'Pokemon'
+})
