e95ff4234d0a9ce3fe308c2b4f69233c9fd5ea31
jonpfluger <jonpfluger718@gmail.com>
2023-08-24 19:24:04 -0500

    sequelize RESTFUL pokemon routes

--- server.js
+++ server.js
@@ -1,60 +1,74 @@
 const sequelize = require('./db/connection')
-const { Pokemon } = require('./models')
+require('./models')
+const routes = require('./routes')
+
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(routes)
 
 // TODO: make false before deploying to heroku
-sequelize.sync({ force: true }).then(async () => {
-
-  // create
-
-  await Pokemon.create({
-    name: 'Pikachu',
-    type: 'electric',
-    isEvolved: true,
+sequelize.sync({ force: false }).then(async () => {
+  console.log('DB synced!')
+  app.listen(PORT, () => {
+    console.log(`Pokemon server listening at http://localhost:${PORT}`)
   })
-
-  await Pokemon.create({
-    name: 'Raichu',
-    type: 'electric',
-    isEvolved: true,
-  })
-
-  await Pokemon.create({
-    name: 'Jigglypuff',
-    type: 'fairy',
-    isEvolved: false,
-  })
-
-  // update
-
-  await Pokemon.update({
-    name: 'Bulbasaur',
-    type: 'grass'
-  }, {
-    raw: true,
-    where: {
-      id: 2
-    }
-  })
-
-  // delete
-
-  await Pokemon.destroy({
-    where: {
-      id: 2
-    }
-  })
-
-  // read
-
-  const allPokemon = await Pokemon.findAll({
-    // where: {
-    //   type: 'electric',
-    //   isEvolved: true,
-    // },
-    raw: true,
-  })
-  console.log(allPokemon)
-
-  const secondPikachu = await Pokemon.findByPk(2, { raw: true })
-  console.log(secondPikachu)
 })
+
+//   // create
+
+//   await Pokemon.create({
+//     name: 'Pikachu',
+//     type: 'electric',
+//     isEvolved: false,
+//   })
+
+//   await Pokemon.create({
+//     name: 'Raichu',
+//     type: 'electric',
+//     isEvolved: true,
+//   })
+
+//   await Pokemon.create({
+//     name: 'Jigglypuff',
+//     type: 'fairy',
+//     isEvolved: false,
+//   })
+
+//   // update
+
+//   await Pokemon.update({
+//     name: 'Bulbasaur',
+//     type: 'grass'
+//   }, {
+//     where: {
+//       id: 2
+//     },
+//     raw: true,
+//   })
+
+//   // delete
+
+//   await Pokemon.destroy({
+//     where: {
+//       id: 2
+//     }
+//   })
+
+//   // read
+
+//   const allPokemon = await Pokemon.findAll({
+//     // where: {
+//     //   type: 'electric',
+//     //   isEvolved: true,
+//     // },
+//     raw: true,
+//   })
+//   console.log(allPokemon)
+
+//   const secondPikachu = await Pokemon.findByPk(2, { raw: true })
+//   console.log(secondPikachu)
