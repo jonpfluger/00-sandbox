6104612c8931b7421576b94ea265177cca591fc9
jonpfluger <jonpfluger718@gmail.com>
2023-08-29 19:31:55 -0500

    seeds, config, db

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 // Dependencies
 const express = require('express');
+const { engine } = require('express-handlebars')
 const path = require('path');
 
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
 
 app.use(express.static(path.join(__dirname, 'public')));
 // Sets up the routes
