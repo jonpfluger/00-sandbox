8d122acb6bb73896251b539551a4f1c6037461d2
jonpfluger <jonpfluger718@gmail.com>
2023-08-29 19:49:25 -0500

    used handlebars to dynamically render

--- controllers/dish-routes.js
+++ controllers/dish-routes.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const path = require('path');
 
+const dishes = require('../seeds/dish-seeds.json')
+
 router.get('/', async (req, res) => {
-  res.render('home')
+  res.render('home', { dishes })
 })
 
-router.get('/dish/:slug', async (req, res) => {
+router.get('/dish/:slug', (req, res) => {
   res.sendFile(path.join(__dirname, `../views/${req.params.slug}.html`));
 });
 
