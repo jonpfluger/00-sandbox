56d95bfa16187dd025344c75e509b807e9408c93
jonpfluger <jonpfluger718@gmail.com>
2023-10-02 19:14:23 -0500

    creates instance of an item in server, but nothing gets saved int he db

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 const connection = require('./config/connection')
+const { Department, Item } = require('./models')
+
+const item = new Item({
+  name: "Bananas",
+  price: 0.67,
+  quantity: 100,
+  expirationDate: new Date('2023-10-20')
+})
+
+console.log(item)
