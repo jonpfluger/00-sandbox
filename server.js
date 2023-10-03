6f4d0863795dea6f53eda82e7b49b97dc10f7bc7
jonpfluger <jonpfluger718@gmail.com>
2023-10-02 19:22:24 -0500

    find method

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const connection = require('./config/connection')
 const { Department, Item } = require('./models')
 
-Item.create({
-  name: "Raisins",
-  price: 0.01,
-  quantity: 10000,
-  expirationDate: new Date('2023-10-27')
-}).then(savedItem => console.log(savedItem))
+// Item.create({
+//   name: "Raisins",
+//   price: 0.01,
+//   quantity: 10000,
+//   expirationDate: new Date('2023-10-27')
+// }).then(savedItem => console.log(savedItem))
+
+Item.find({ name: 'Bananas' })
+  .then(item => console.log(item))
