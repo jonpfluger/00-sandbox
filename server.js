f533251d2b495db8ca67f7f7a462dfe5ad7302a7
jonpfluger <jonpfluger718@gmail.com>
2023-10-02 21:18:37 -0500

    aggregations

--- server.js
+++ server.js
@@ -3,18 +3,47 @@ const connection = require('./config/connection')
 const { Department, Item } = require('./models')
 
 const init = async () => {
-  const dept = await Department.create({
-    name: "Produce",
-    employees: [
-      { name: 'Josh' },
-      { name: 'David' },
-      { name: 'Max' },
-    ],
-  })
 
-  console.log(`There are ${dept.getEmployeeCount()} employees in this department.`)
+  // const allItems = await Item.find()
+  // console.log(allItems)
 
-  dept.alertCleanup()
+  // let maxPrice = 0
+
+  // for (const item of items) {
+  //   if (item.price > maxPrice) {
+  //     maxPrice = item.price
+  //   }
+  // }
+
+  // console.log(maxPrice)
+
+  const result = await Item.aggregate([
+    {
+      $match: {
+        quantity: { $gte: 20 }
+      }
+    },
+    {
+      $group: {
+        _id: null,
+        maxPrice: { $max: '$price' },
+        allPricesTotal: { $sum : '$price' }
+      }
+    }
+  ])
+
+  // const dept = await Department.create({
+  //   name: "Produce",
+  //   employees: [
+  //     { name: 'Josh' },
+  //     { name: 'David' },
+  //     { name: 'Max' },
+  //   ],
+  // })
+
+  // console.log(`There are ${dept.getEmployeeCount()} employees in this department.`)
+
+  // dept.alertCleanup()
 
 }
 
