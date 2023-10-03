f323bcf50b1fdbbaf67f29b9c733f64155eb120b
jonpfluger <jonpfluger718@gmail.com>
2023-10-02 19:38:14 -0500

    create and read

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const connection = require('./config/connection')
 const { Department, Item } = require('./models')
 
+// create
+
 // Item.create({
 //   name: "Raisins",
 //   price: 0.01,
@@ -8,5 +10,7 @@ const { Department, Item } = require('./models')
 //   expirationDate: new Date('2023-10-27')
 // }).then(savedItem => console.log(savedItem))
 
-Item.find({ name: 'Bananas' })
-  .then(item => console.log(item))
+// read
+
+// Item.find({ name: 'Bananas' }, 'price quantity') // -expirationDate
+//   .then(item => console.log(item))
