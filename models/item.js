fbd533040f66db2fbfc27b98a8e7b8b53074cc5f
jonpfluger <jonpfluger718@gmail.com>
2023-10-02 19:00:57 -0500

    sets up item schema

--- models/item.js
+++ models/item.js
@@ -1,1 +1,24 @@
-// item
+const { Schema } = require('mongoose')
+
+const itemSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  quantity: {
+    type: Number,
+    default: 0,
+  },
+  // TODO: dynamic default date
+  expirationDate: {
+    type: Date,
+    // default: Date.now,
+  },
+})
+
+module.exports = itemSchema
