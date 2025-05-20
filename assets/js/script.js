04c7e09bb1924e64dbd905c8c4e0cc1aa4aa70e1
jonpfluger <jonpfluger718@gmail.com>
2025-05-20 17:39:25 -0500

    adds javascript click listener to button, adds script tag to html

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const modeBtnEl = document.querySelector('#toggle')
+
+const handleModeToggle = function () {
+    console.log('Mode button clicked!')
+}
+
+modeBtnEl.addEventListener('click', handleModeToggle);
