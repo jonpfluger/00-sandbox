8cb99a42dcf0fe1a8cf3fc2a668db3a19cec97f7
jonpfluger <jonpfluger718@gmail.com>
2025-05-21 22:53:00 -0500

    adds readMode and saveMode functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
 const modeBtnEl = document.querySelector('#toggle')
 
+const readMode = function () {
+    const mode = localStorage.getItem('mode') || 'dark'
+    return mode
+}
+
+const saveMode = function (mode) {
+    localStorage.setItem('mode', mode)
+}
+
 const handleModeToggle = function () {
-    console.log('Mode button clicked!')
+    const mode = readMode()
+
+    console.log('mode', mode);
+
+    // saveMode()
 }
 
 modeBtnEl.addEventListener('click', handleModeToggle);
